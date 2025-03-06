d5a8256045248f2ad2d72b16e25dd53c54c55407
Enoch Amedjrovi <enoch.amedjrovi@stu.ocsb.ca>
2025-03-06 05:11:34 +0000

    temp.py

---
+++ temp.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+# Created By:Enoch Amedjrovi
+# 3rd March 3rd, 2025
+# This program list the math library
+
+import math
+
+Val = 3.14
+sqrtVal = math.sqrt(Val)
+print(sqrtVal) #sqrtVal^2
+
+expVal = math.exp(Val)#e^(val)
+
+print(expVal)
+
+factVal = math.factorial(math.floor(Val)) #5! =
+
+print(factVal)
+
+sinVal = math.sin(Val)
+print(sinVal)
+
+floorVal = math.floor(Val)
+print(floorVal)
+
+ceilingVal = math.ceil(Val)
+print(ceilingVal)
